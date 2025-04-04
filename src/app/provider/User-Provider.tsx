63165f6cef790a07cc33eb2970ba345a56bf0fe2
Byambasuren11 <byumbasurenbold@gmail.com>
2025-04-04 19:12:53 +0800

    ho

--- src/app/provider/User-Provider.tsx
+++ src/app/provider/User-Provider.tsx
@@ -10,14 +10,11 @@ const UserContext = createContext<userContextType>({} as userContextType);
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<userContextType>({} as userContextType);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
     const user1 = localStorage.getItem("user");
     if (user1) {
       const user = JSON.parse(user1);
-      setLoading(false);
       setUser(user);
     }
   }, []);
@@ -26,7 +23,7 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
     <UserContext.Provider
       value={{ email: user?.email, addrress: user.addrress }}
     >
-      {loading ? <div>Loading</div> : children}
+      {children}
     </UserContext.Provider>
   );
 };
